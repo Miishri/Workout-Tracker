970004d6a9e6635d27f3ac919a46fed072473cf2
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-30 17:35:58 +0200

    add findbyexerciseignorecase method for jpa

--- src/main/java/org/gym/workouttracker/repositories/ExerciseRepository.java
+++ src/main/java/org/gym/workouttracker/repositories/ExerciseRepository.java
@@ -3,7 +3,9 @@ package org.gym.workouttracker.repositories;
 import org.gym.workouttracker.entities.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, UUID> {
+    List<Exercise> findExercisesByExerciseNameIgnoreCase(String exerciseName);
 }
