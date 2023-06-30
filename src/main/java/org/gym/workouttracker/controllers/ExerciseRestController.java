1c57033d8e31cb830d8ffd2a00cfbad4eb202ba4
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-30 19:28:37 +0200

    implement create endpoint

--- src/main/java/org/gym/workouttracker/controllers/ExerciseRestController.java
+++ src/main/java/org/gym/workouttracker/controllers/ExerciseRestController.java
@@ -1,7 +1,14 @@
 package org.gym.workouttracker.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.gym.workouttracker.models.ExerciseDTO;
+import org.gym.workouttracker.repositories.ExerciseRepository;
 import org.gym.workouttracker.services.ExerciseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/exercises")
 public class ExerciseRestController {
     private final ExerciseService exerciseService;
+    private final ExerciseRepository exerciseRepository;
+
+    @PostMapping("/add")
+    public ResponseEntity<ExerciseDTO> createExercise(@RequestBody @Validated ExerciseDTO exerciseDTO) {
+        ExerciseDTO createdDto = exerciseService.createExercise(exerciseDTO);
+        return new ResponseEntity<>(
+                createdDto,
+                HttpStatus.CREATED
+                );
+    }
 }
