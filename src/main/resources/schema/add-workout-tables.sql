8b83db7fea30457ea758d663a0ca49171da6fbec
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-07-03 23:18:56 +0200

    fix type

--- src/main/resources/schema/add-workout-tables.sql
+++ src/main/resources/schema/add-workout-tables.sql
@@ -1,10 +1,9 @@
-
-
 CREATE TABLE exercises (
-    exercise_id 			varchar(36)      NOT NULL,
-    exercise_name 			varchar(100) 	 NOT NULL,
-    exercise_details 		varchar(150)	 NOT NULL,
-    recent_exercise_sets	int 			 DEFAULT 0,
-    exercise_created		datetime(6) 	 NOT NULL,
-    PRIMARY KEY             (exercise_id)
+                           exercise_id 			varchar(36)      NOT NULL,
+                           exercise_name 			varchar(100) 	 NOT NULL,
+                           exercise_details 		varchar(150)	 NOT NULL,
+                           recent_exercise_sets	    int 	 DEFAULT 0,
+                           personal_record          DECIMAL(10, 2) 	 DEFAULT 0,
+                           exercise_created		    datetime(6) 	 NOT NULL,
+                           PRIMARY KEY             (exercise_id)
 )
