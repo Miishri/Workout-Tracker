dd37733b013d223b94d7f182c7ac4844173cd759
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-07-04 11:47:41 +0200

    implement crud operations

--- src/main/java/org/gym/workouttracker/controllers/ExerciseRestController.java
+++ src/main/java/org/gym/workouttracker/controllers/ExerciseRestController.java
@@ -2,23 +2,22 @@ package org.gym.workouttracker.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.gym.workouttracker.models.ExerciseDTO;
-import org.gym.workouttracker.repositories.ExerciseRepository;
 import org.gym.workouttracker.services.ExerciseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/exercises")
 public class ExerciseRestController {
+
     private final ExerciseService exerciseService;
-    private final ExerciseRepository exerciseRepository;
 
     @PostMapping("/add")
     public ResponseEntity<ExerciseDTO> createExercise(@RequestBody @Validated ExerciseDTO exerciseDTO) {
@@ -28,4 +27,37 @@ public class ExerciseRestController {
                 HttpStatus.CREATED
                 );
     }
+
+    @GetMapping("/list")
+    public List<ExerciseDTO> getAllExercises(@RequestBody(required = false) String exerciseName) {
+        return exerciseService.getExercises(exerciseName);
+    }
+
+    @GetMapping("/{exerciseId}")
+    public ResponseEntity<ExerciseDTO> getExerciseById(@PathVariable("exerciseId") UUID exerciseId) {
+        return new ResponseEntity<>(
+                exerciseService.getExerciseById(exerciseId),
+                HttpStatus.FOUND
+        );
+    }
+
+    @PutMapping("/edit/{exerciseId}")
+    public ResponseEntity<ExerciseDTO> updateExercise(@PathVariable("exerciseId") UUID exerciseId,
+                                                      @RequestBody @Validated ExerciseDTO exerciseDTO) {
+        return new ResponseEntity<>(
+                exerciseService.updateExercise(exerciseId, exerciseDTO),
+                HttpStatus.ACCEPTED
+        );
+    }
+
+    @DeleteMapping("/delete/{exerciseId}")
+    public ResponseEntity<ExerciseDTO> deleteExerciseById(@PathVariable("exerciseId") UUID exerciseId) {
+        return new ResponseEntity<>(
+                exerciseService.deleteExerciseById(exerciseId),
+                HttpStatus.ACCEPTED
+        );
+    }
+
+
+
 }
